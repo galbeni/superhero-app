16acbb19cc70f2e49c089a6ec62a932ada179393
Gál Beni <beniauthor@gmail.com>
2025-04-11 01:03:35 +0200

    refactor: update Filter, Card, and Header components for improved structure and styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,33 +1,33 @@
-import { IHeader } from "../../types";
+interface IHeader {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+}
 
 export const Header = ({ activeTab, setActiveTab }: IHeader ) => {
   const buttonStyle = {
-    base: "p-3 cursor-pointer border-gray-400 dark:border-white text-white",
+    base: "rounded-md px-3.5 py-2.5 text-sm font-semibold text-white cursor-pointer shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400",
     active: "font-bold bg-sky-500/100",
     inactive: "bg-sky-500/75",
   };
 
-  // Switch active tab
   const switchTab = (tab: string) => {
     setActiveTab(tab);
   };
 
   return (
-    <>
-      <div className="flex">
-        <button 
-          onClick={() => switchTab("heroes")} 
-          className={`${buttonStyle.base} ${activeTab === "heroes" ? buttonStyle.active : buttonStyle.inactive}`}
+    <div className="flex gap-4">
+      <button 
+        onClick={() => switchTab("heroes")} 
+        className={`${buttonStyle.base} ${activeTab === "heroes" ? buttonStyle.active : buttonStyle.inactive}`}
         >
-          Heroes
-        </button>
-        <button 
-          onClick={() => switchTab("favorites")} 
-          className={`${buttonStyle.base} ${activeTab === "favorites" ? buttonStyle.active : buttonStyle.inactive}`}
+        All Heroes
+      </button>
+      <button 
+        onClick={() => switchTab("favorites")} 
+        className={`${buttonStyle.base} ${activeTab === "favorites" ? buttonStyle.active : buttonStyle.inactive}`}
         >
-          Favorites
-        </button>
-      </div>
-    </>
+        Favorites
+      </button>
+    </div>
   );
 };
