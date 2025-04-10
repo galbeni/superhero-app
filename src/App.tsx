df7233938503de478db635186c7912d90cb7e86c
Gál Beni <beniauthor@gmail.com>
2025-04-11 01:02:22 +0200

    refactor: App component to use context providers for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,222 +1,68 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { Header } from './components/header/Header';
-import { Card } from './components/card/Card';
+import { HeroSection } from './components/HeroSection.tsx';
 import { Filter } from './components/Filter';
-import { IAppStates, IHero } from './types';
+import { useHeroesContext } from './context/heroes/useHeroesContext';
+import { useFavoritesContext } from './context/favorites/useFavoritesContext';
+import { useUIContext } from './context/ui/useUIContext';
+import { HeroList } from './components/lists/HeroList';
+import { FavoriteList } from './components/lists/FavoriteList.tsx';
+import { Spinner } from './components/Spinner.tsx';
 
 function App() {
-  const initialState: IAppStates = {
-    filterBy: "show-all",
-    sortBy: "default",
-    activeTab: "heroes",
-    heroes: [],
-    fullHeroes: [],
-    publishers: [],
-    powerstats: [],
-    favorites: [],
-    isFavorite: false,
-    loading: false,
-    currentPage: 1,
-    heroesPerPage: 10,
-    totalHeroes: 0,
-  };
+  const {
+    loading,
+    powerstats,
+    publishers
+  } = useHeroesContext();
 
-  const [state, setState] = useState<IAppStates>(initialState);
-  const apiUrl = import.meta.env.VITE_API_URL as string;
+  const {
+    favorites
+  } = useFavoritesContext();
 
-// Fetch heroes from API with useEffect promise and async/await syntax with error handling
-  const fetchHeroes = async () => {
-    try {
-      setState(prev => ({ ...prev, loading: true }));
-
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-  
-      const start = (state.currentPage - 1) * state.heroesPerPage;
-      const end = start + state.heroesPerPage;
-      const paginatedData = data.slice(start, end);
-
-      const publishers = data.reduce((acc: string[], hero: { biography: { publisher: string } }) => {
-        const publisher = hero.biography.publisher;
-
-        if (publisher && !acc.includes(publisher)) {
-          acc.push(publisher);
-        }
-
-        return acc.sort(); // Sort publishers alphabetically
-      }, []);
-
-      const powerstats = data.reduce((acc: string[], hero: { powerstats: { [key: string]: string } }) => {
-        const stats = Object.keys(hero.powerstats);
-
-        stats.forEach(stat => {
-          if (!acc.includes(stat)) acc.push(stat);
-        });
-
-        return acc.sort();
-      }, []);
-
-      setState(prev => ({
-        ...prev,
-        fullHeroes: data,
-        heroes: [...prev.heroes, ...paginatedData],
-        publishers,
-        powerstats,
-        totalHeroes: data.length,
-        loading: false
-      }));
-    } catch (error) {
-      console.error("Error fetching heroes:", error);
-      setState(prev => ({ ...prev, loading: false }));
-    }
-  };
-
-  useEffect(() => {
-    fetchHeroes();
-  }, []);
-
-  // Load favorites from localStorage with useEffect and JSON.parse
-  useEffect(() => {
-    const storedFavorites = localStorage.getItem("favorites");
-  
-    if (storedFavorites) {
-      try {
-        const parsedFavorites = JSON.parse(storedFavorites);
-        setState(prev => ({ ...prev, favorites: parsedFavorites }));
-      } catch (error) {
-        console.error("Failed to parse favorites from localStorage:", error);
-      }
-    }
-  }, []);
-
-  // Filter heroes by publisher
-  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedPublisher = event.target.value;
-    setState(prev => ({ ...prev, filterBy: selectedPublisher }));
-  };
-
-  // Sort heroes by powerstats
-  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedSort = event.target.value;
-    setState(prev => ({ ...prev, sortBy: selectedSort }));
-  };
-
-  // Adding/removing a hero from favorites and persisting in localStorage
-  const toggleFavorite = (hero: IHero) => {
-    const { favorites } = state;
-    const isFavorite = favorites.some(fav => fav.id === hero.id);
-
-    let updatedFavorites;
-
-    if (isFavorite) {
-      updatedFavorites = favorites.filter(fav => fav.id !== hero.id); // Remove from favorites
-    } else {
-      updatedFavorites = [...favorites, hero]; // Add to favorites
-    }
-
-    setState(prev => ({ ...prev, favorites: updatedFavorites }));
-    localStorage.setItem("favorites", JSON.stringify(updatedFavorites)); // Persist favorites in localStorage
-  };
-
-  const filteredFull = state.filterBy === "show-all"
-    ? state.fullHeroes
-    : state.fullHeroes.filter(hero => hero.biography.publisher === state.filterBy);
-
-  const filteredHeroes = filteredFull
-    .slice(0, state.currentPage * state.heroesPerPage) // Pagination
-    .sort((a, b) => {
-      const stat = state.sortBy;
-      if (stat === "default") return 0;
-
-      const aStat = a.powerstats?.[stat as keyof typeof a.powerstats] ?? 0;
-      const bStat = b.powerstats?.[stat as keyof typeof b.powerstats] ?? 0;
-
-      return bStat - aStat; // Descending order
-    });
-
-  const resetFilter = () => {
-    setState(prev => ({ ...prev, filterBy: "show-all", sortBy: "default" }));
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const bottom =
-        window.innerHeight + window.scrollY >= document.body.offsetHeight - 10;
-  
-      if (bottom && !state.loading && state.heroes.length < state.totalHeroes) {
-        setState((prev) => ({
-          ...prev,
-          currentPage: prev.currentPage + 1,
-        }));
-      }
-    };
-  
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [state.loading, state.heroes.length, state.totalHeroes]);
+  const {
+    activeTab,
+    filterBy,
+    handleFilterChange,
+    handleSortChange,
+    resetFilter,
+    sortBy
+  } = useUIContext();
 
   return (
-    <>
-      <div className="flex bg-indigo-100 w-screen min-h-screen">
-        <div className="container ml-auto mr-auto flex flex-col flex-wrap items-start">
-          <div className="w-full pl-5 lg:pl-2 mb-4 mt-4">
-            <h1 className="text-center text-4xl lg:text-4xl text-gray-700 font-extrabold">Superhero.App</h1>
-            <h2 className="text-center text-3xl">
-              {state.activeTab === "heroes" ? "Heroes" : state.activeTab === "favorites" ? "Favorites" : ""}
-            </h2>
-          </div>
-          <Header
-            activeTab={state.activeTab}
-            setActiveTab={(tab: string) =>
-              setState(prev => ({ ...prev, activeTab: tab }))
-            }
-          />
-          <hr className="w-full p-2 border-gray-400 dark:border-white" />
-          {state.activeTab === "heroes" && (
-            <>
-              <Filter
-                filterBy={state.filterBy}
-                sortBy={state.sortBy}
-                handleFilterChange={handleFilterChange}
-                handleSortChange={handleSortChange}
-                publishers={state.publishers}
-                powerstats={state.powerstats}
-                resetFilter={resetFilter}
-              />
-              <div className="w-full flex flex-wrap">
-                {filteredHeroes.map((hero: IHero) => (
-                  <Card
-                    key={hero.id}
-                    hero={hero}
-                    toggleFavorite={() => toggleFavorite(hero)}
-                    isFavorite={state.favorites.some(fav => fav.id === hero.id)}
-                  />
-                ))}
-              </div>
-            </>
-          )}
-          {state.activeTab === "favorites" && (
-            <>
-              <div className="w-full flex flex-wrap">
-                {state.favorites.length > 0 ? (
-                  state.favorites.map((hero: IHero) => (
-                    <Card
-                      key={hero.id}
-                      hero={hero}
-                      toggleFavorite={() => toggleFavorite(hero)}
-                      isFavorite={true}
-                    />
-                  ))
+    <div className="relative isolate overflow-hidden bg-gray-900">
+      <div className="flex h-screen items-center justify-center">
+        <HeroSection />
+        <div className="flex bg-black w-screen min-h-screen">
+          <div className="container ml-auto mr-auto flex flex-col flex-wrap items-start">
+            {activeTab === "heroes" && (
+              <>
+                {<Filter
+                  filterBy={filterBy}
+                  sortBy={sortBy}
+                  handleFilterChange={handleFilterChange}
+                  handleSortChange={handleSortChange}
+                  publishers={publishers}
+                  powerstats={powerstats}
+                  resetFilter={resetFilter}
+                />}
+                {loading ? (
+                  <Spinner />
                 ) : (
-                  <p>No favorites yet...</p>
+                  <HeroList />
                 )}
-              </div>
-            </>
-          )}
+              </>
+            )}
+            {activeTab === "favorites" && (
+              favorites.length > 0 ? (
+                <FavoriteList />
+              ) : (
+                <p className="text-center w-full py-4 text-gray-600">No favorites yet...</p>
+              )
+            )}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HeroesProvider } from './context/heroes/heroesProvider';
+import { FavoritesProvider } from './context/favorites/favoritesProvider';
+import { UIProvider } from './context/ui/uiProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroesProvider>
+      <FavoritesProvider>
+        <UIProvider>
+          <App />
+        </UIProvider>
+      </FavoritesProvider>
+    </HeroesProvider>
   </StrictMode>,
 )
